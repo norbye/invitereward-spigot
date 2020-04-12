f77f486d5520afbfac8f3e7cbb7ffd89dd062961
Jonatan Norbye <jonatan@norbye.com>
2020-04-13 00:54:58 +0200

    Updated db structure and updated functionality for users to redeem invite rewards

--- src/main/java/com/norbye/dev/invitereward/CommandInviteReward.java
+++ src/main/java/com/norbye/dev/invitereward/CommandInviteReward.java
@@ -1,5 +1,6 @@
 package com.norbye.dev.invitereward;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -37,7 +38,7 @@ public class CommandInviteReward implements CommandExecutor {
             plugin.error("Failed to stop mysql connection on reload");
             e.printStackTrace();
         }
-        plugin.initialiseDBConnection();
+        plugin.initializeDBConnection();
         // Notify console
         PluginDescriptionFile pdf = plugin.getDescription();
         if (commandSender instanceof Player) {
@@ -47,7 +48,7 @@ public class CommandInviteReward implements CommandExecutor {
         // Notify sender
         commandSender.sendMessage(ChatColor.GOLD + "[" + pdf.getName() + "] v" + pdf.getVersion());
         commandSender.sendMessage(ChatColor.GOLD + pdf.getName() + " reloaded");
-        return false;
+        return true;
     }
 
     private boolean onRewardCommand(CommandSender commandSender, String[] args) {
@@ -68,21 +69,67 @@ public class CommandInviteReward implements CommandExecutor {
             return true;
         }
 
-        // TODO update the db connection stuffs
         // Check if the reward code is active in the db
         String rewardCode = args[0];
+        String rewardCommand = "";
         try {
-            String sql = "SELECT * FROM invitations WHERE Something='Something'";
+            String sql = "SELECT i.`used`, c.`command` FROM invitations AS i " +
+                    "LEFT JOIN `commands` AS c ON i.`command_id` = c.`command_id` " +
+                    "WHERE i.`invite_id`=? AND c.active=1";
             PreparedStatement stmt = plugin.connection.prepareStatement(sql);
+            stmt.setString(1, rewardCode);
             ResultSet results = stmt.executeQuery();
             if (!results.next()) {
-                System.out.println("Failed");
+                plugin.debug("Invalid reward code");
+                commandSender.sendMessage("Invalid reward code");
+                return true;
             } else {
-                System.out.println("Success");
+                if (results.getTimestamp("used") != null) {
+                    commandSender.sendMessage("The reward code inserted has already been used");
+                    return true;
+                }
+                rewardCommand = results.getString("command");
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            commandSender.sendMessage("Failed to verify reward code");
+            return true;
         }
-        return false;
+        if ("".equals(rewardCommand)) {
+            commandSender.sendMessage("Failed to execute reward");
+            return true;
+        }
+        if (rewardCommand == null) {
+            commandSender.sendMessage("Invalid reward");
+            return true;
+        }
+        // Replace variables
+        rewardCommand = rewardCommand.replaceAll("\\{playername\\}", commandSender.getName());
+        commandSender.sendMessage("Command: /" + rewardCommand);
+        // Update db that the command was executed
+        try {
+            String sql = "UPDATE `invitations` SET `used`=?, `used_by`=? WHERE `invite_id`=?";
+            PreparedStatement stmt = plugin.connection.prepareStatement(sql);
+            stmt.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+            stmt.setString(2, String.valueOf(((Player) commandSender).getUniqueId()));
+            stmt.setString(3, rewardCode);
+            int updatedIndex = stmt.executeUpdate();
+            if (updatedIndex > 0) {
+                plugin.debug("Successfully registered reward as used");
+                // Execute command
+                plugin.log("Command executed: /" + rewardCommand);
+                Bukkit.getServer().dispatchCommand(
+                        Bukkit.getServer().getConsoleSender(),
+                        rewardCommand
+                );
+            } else {
+                plugin.error("Failed to register reward as used");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            plugin.error("Failed to cancel code");
+            return true;
+        }
+        return true;
     }
 }
--- src/main/java/com/norbye/dev/invitereward/Main.java
+++ src/main/java/com/norbye/dev/invitereward/Main.java
@@ -6,10 +6,7 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main extends JavaPlugin {
 
@@ -31,7 +28,7 @@ public class Main extends JavaPlugin {
         this.getCommand("invitereward").setExecutor(new CommandInviteReward(this));
 
         // Launch the mysql connections
-        initialiseDBConnection();
+        initializeDBConnection();
     }
 
     @Override
@@ -45,7 +42,7 @@ public class Main extends JavaPlugin {
         }
     }
 
-    public void initialiseDBConnection() {
+    public void initializeDBConnection() {
         final String username = config.getString("db.user", "");
         final String password = config.getString("db.pass", "");
         final String url = "jdbc:mysql://"
@@ -60,7 +57,6 @@ public class Main extends JavaPlugin {
         }
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            debug("mySQL driver exists");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             error("jdbc driver unavailable!");
@@ -73,18 +69,63 @@ public class Main extends JavaPlugin {
                     username,
                     password
             );
-            debug("Connection established");
+            debug("mySQL connection established");
 
-            // TODO update the table setup
-            // Default db setup
-            String sql = "CREATE TABLE IF NOT EXISTS invites(invite_id char(7))";
-            PreparedStatement stmt = connection.prepareStatement(sql);
-            stmt.executeUpdate();
-            debug("Default table created");
+            updateDBStructure();
+
             // For more stuffs: https://www.spigotmc.org/wiki/mysql-database-integration-with-your-plugin/
         } catch (SQLException e) {
             error("Failed to setup SQL connection");
             e.printStackTrace();
+        }
+    }
+
+    private void updateDBStructure() throws SQLException {
+        // `db`.`invitations`
+        String sql = "CREATE TABLE IF NOT EXISTS invitations(" +
+                "`invite_id` char(7) primary key not null, " +
+                "`command_id` int(11) not null," +
+                "`expires` datetime default null," +
+                "`used` datetime default null," +
+                "`used_by` char(36) default null" +
+                ")";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.executeUpdate();
+        // `db`.`commands`
+        sql = "CREATE TABLE IF NOT EXISTS `commands`(" +
+                "`command_id` int(11) auto_increment primary key," +
+                "`command` text not null," +
+                "`active` tinyint(1) default 1" +
+                ")";
+        stmt = connection.prepareStatement(sql);
+        stmt.executeUpdate();
+        // `db`.`invitereward`
+        sql = "CREATE TABLE IF NOT EXISTS `invitereward`(" +
+                "`key` varchar(45) primary key not null," +
+                "`value` text not null" +
+                ")";
+        stmt = connection.prepareStatement(sql);
+        stmt.executeUpdate();
+
+        sql = "SELECT `key`, `value` FROM invitereward WHERE `key` = 'dbversion'";
+        stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        int dbVersion = 0;
+        if (rs.next()) {
+            dbVersion = rs.getInt("value");
+        }
+        debug("dbVersion " + dbVersion);
+        if (dbVersion == 0) {
+            // Insert dbVersion
+            sql = "INSERT INTO invitereward (`key`, `value`) VALUES (?,?)";
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, "dbversion");
+            stmt.setInt(2, 1);
+            if (stmt.executeUpdate() != 0) {
+                debug("Updated dbVersion to 1");
+            } else {
+                debug("Failed to update dbVersion to 1");
+            }
         }
     }
 
